9a9e54e2bd34aa5423cab059ca7edb9ab1dc6531
alinaboi <alinaboyarchuk28@gmail.com>
2021-10-27 21:21:43 +0300

    task 5 was improved

--- tasksJS/hometasks2/5.js
+++ tasksJS/hometasks2/5.js
@@ -1,27 +1,27 @@
 //Write a JavaScript function with conditional statement to sort three numbers .
 
 function threeNumSorting(a, b, c) {
-    let firstResult = [c, b, a];
-    let secondResult = [b, c, a];
-    let thirdResult = [a, b, c];
-    let fourthResult = [b, a, c];
-    let fifthResult = [c, a, b];
-    let sixthResult = [a, c, b];
+    const firstResult = [c, b, a];
+    const secondResult = [b, c, a];
+    const thirdResult = [a, b, c];
+    const fourthResult = [b, a, c];
+    const fifthResult = [c, a, b];
+    const sixthResult = [a, c, b];
 
-    if (a > b && a > c) {
-        if (b > c) {
+    if (a >= b && a>= c) {
+        if (b>= c) {
             return firstResult;
         } else {
             return secondResult;
         }
-    } else if (b > a && b > c) {
-        if (a > c) {
+    } else if (b>= a && b>= c) {
+        if (a>= c) {
             return fifthResult;
         } else {
             return sixthResult;
         }
-    } else if (c > a && c > b) {
-        if (a > b) {
+    } else if (c>= a && c>= b) {
+        if (a>= b) {
             return fourthResult;
         } else {
             return thirdResult;
@@ -37,3 +37,4 @@ console.log(threeNumSorting(4, 0, 1));
 console.log(threeNumSorting(78, 9, 105));
 console.log(threeNumSorting(-2, 22, -2));
 console.log(threeNumSorting(12, 7, "digit"));
+console.log(threeNumSorting(0.5, 0.5, 0.5));
